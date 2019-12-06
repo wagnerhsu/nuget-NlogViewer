5510afc9a823e63f4890c7d03b56016469a65177
Wagner Xu <wagnerhsu@hotmail.com>
2019-12-06 18:08:37 +0800

    Test for .net core version

---
+++ WpfAppDemo/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NLog;
+
+namespace WpfAppDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ILogger Logger = LogManager.CreateNullLogger();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button button = e.Source as Button;
+            if (button == null) return;
+
+            if (button.Name == nameof(btnTest))
+            {
+                Logger.Info("Information");
+                try
+                {
+                    int i = 0;
+                    int j = 1 / i;
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "Error Information");
+                }
+            }
+        }
+    }
+}
